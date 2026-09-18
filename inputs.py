a27e271fffc114dce34973086d5820a873446a26
situmacollins87-ops <situmacollins87@gmail.com>
2026-09-18 16:23:44 +0300

    python_study

---
+++ inputs.py
@@ -0,0 +1,7 @@
+#input function
+my_name=input("enter my_name:")
+print(my_name)
+first_name=input("Enter first_name:")
+last_name=input("Enter last_name:")
+my_name=first_name+last_name
+print(my_name)
